5d589c509b73307847c1f6d13ed300cc3e3786ac
M Umar Adnan <umaradnan2710@gmail.com>
2026-09-17 14:26:46 +0500

    Visualizes the themes and Color Pallets built-in Seaborn

---
+++ Themes and Color pallet.py
@@ -0,0 +1,7 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.set_theme(style='darkgrid')
+sns.scatterplot(x=[1, 2, 3, 4, 5], y=[10, 20, 30, 40, 50])
+sns.color_palette('dark')
+
+plt.show()
